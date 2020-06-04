82f8b13b7df462b00014b3bf4f57ebff64b5ac04
rezkyputra <rezky.putra46@gmail.com>
2020-06-04 11:28:32 +0700

    last

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,12 @@ const { User, List } = require("../models");
 exports.index = async (req, res) => {
   try {
     const users = await User.findAll({
+      include: [
+        {
+          model: List,
+          attributes: { exclude: ["createdAt", "updatedAt"] },
+        },
+      ],
       attributes: { exclude: ["createdAt", "updatedAt", "ListId", "listId"] },
     });
     res.send({ data: users });
