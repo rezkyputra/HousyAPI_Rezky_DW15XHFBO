620c39dfd8e934ed8bab247c37ac201b3dfe1c1f
rezkyputra <rezky.putra46@gmail.com>
2020-06-04 12:30:40 +0700

    last

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const router = require("./routes");
-require("dotenv").config();
 
 app.use(cors());
-
 app.use(express.json());
 app.use("/api/v1", router);
+
+app.get("/", (req, res) => {
+  res.send("Connect Success");
+});
 
 app.listen(port, () => console.log(`Listen on port ${port}!`));
